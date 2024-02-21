29331e7517cd63abdef12181bc5bce6e10601c99
WantanXD <rechngml@gmail.com>
2024-02-21 23:23:07 +0900

    多少の修正

--- routers/gemini.js
+++ routers/gemini.js
@@ -9,28 +9,31 @@ router.post("/generateQuestion", async(req, res) => {
     const { pl } = req.body;
 
     // 問題を生成
-    const generateQuestionPrompt = `プログラミング言語"${pl}"のquestionを1つ出力して。questionとは、プログラミング言語${pl}の知識を試すものであるとし、問題文`;
+    const generateQuestionPrompt = `プログラミング言語"${pl}"のquestionを1つ出力して。questionとは、プログラミング言語${pl}の知識を試すものであるとする。生成した問題に対する模範解答も生成して。ただし、学生に解答が知られてしまうといけないので、必ず問題と模範解答を"---"で区切ること。`;
 
     const generateQuestion = await model.generateContentStream(generateQuestionPrompt);
 
-    const responce = await generateQuestion.response;
+    const responce = await (await generateQuestion.response).text().split("---");
+
     const returnData = {
-        question: responce.text()
+        question: responce[0],
+        modelAnswer: responce[1]
     }
 
     return res.json({ returnData });
 });
 
+// 模範解答を生成する
+
 // 解答の正誤判定
 router.post("/answerCheck", async(req, res) => {
 
-    const {question, answer} = req.body;
-    const MAX_COMMENTS = 4;
+    const {question, answer, modelAnswer, pl} = req.body;
     
     // クエリパラメータから問題文に対する回答を出力
     // 問題文に対して、ユーザの記述したプログラムが正しいかどうかをAiが判断する
-    const answerCheckPrompt = `問題文「${question}」を解答「${answer}」が満たす場合は"Y"を出力して。そうでない場合は"Great"を表示して。`;
-    const descriptionPrompt = `問題文「${question}」に対する解説、また「${answer}」以外の簡単な別解、その別解の解説、合計3項目を出力して。ただし、必ずそれぞれの項目の間に特殊文字"---"を入れて。`;
+    const answerCheckPrompt = `プログラミング言語${pl}に関する問題文「${question}」に対する模範解答「${modelAnswer}」を表す内容として、ユーザの解答「${answer}」が正しい場合は"Y"を出力して。そうでない場合は"Great"を表示して。`;
+    const descriptionPrompt = `プログラミング言語${pl}に関する問題文「${question}」の模範解答「${modelAnswer}」を生成して。また、問題文「${question}」そのものに対する解説を生成して。もし存在するなら「${answer}」以外の簡単な別解、その別解の解説を出力して。ただし、必ずそれぞれの項目の間に特殊文字"---"を入れて。`;
 
     const answerCheck = await model.generateContentStream(answerCheckPrompt);
     const description = await model.generateContentStream(descriptionPrompt);
