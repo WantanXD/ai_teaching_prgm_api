d27dcd7b9a1e51b3d0f670a6e390383e9dbd2bc5
WantanXD <rechngml@gmail.com>
2024-02-22 13:46:55 +0900

    認証周りのjwt実装、デバッグなし

---
+++ routers/db.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const jwt = require('./jwtHelper');
+
+router.post('/QandARegister', async(req, res) => {
+  
+  const {lang, question, answer, tof, modelAnswer, comment} = req.body;
+  let userId;
+  const jwtKey = process.env.SECRET_JWT;
+
+  // ユーザIdをjwtから取得
+  const token = localStorage.getItem('jwt');
+  if (token !== null) {
+    try {
+      const decodedToken = verify(token, jwtKey);
+
+      userId = await prisma.authenticate.findFirst({
+        id,
+        where: {
+          userId: decodedToken.id,
+        },
+      });
+    }catch(error) {
+    }
+  }
+  
+  const QandAData = await prisma.qandadata.create ({
+    data : {
+      userId,
+      lang,
+      tof,
+      question,
+      answer,
+      model_answer: modelAnswer,
+      comment,
+    }
+  });
+
+  return res.json({ QandAData });
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const cors = require("cors");
 require("dotenv").config();
 const authRoute = require("./routers/auth");
 const geminiRoute = require("./routers/gemini");
+const dbRoute = require('./routers/db');
 
 const PORT = 4649;
 
 app.use(express.json());
 app.use(cors());
+
 app.use("/api/auth", authRoute);
 app.use("/api/gemini", geminiRoute);
+app.use("/api/db", dbRoute);
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
